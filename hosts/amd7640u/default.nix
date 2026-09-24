170d9252a9bf050be00b743fadd76a4d40f9ac09
brokenpike <brokenpike@garmr.org>
2026-09-24 08:51:38 +0200

    update amd7640u kernel to unstable
    Use the latest nixpkgs kernel package for the AMD7640U host.

--- hosts/amd7640u/default.nix
+++ hosts/amd7640u/default.nix
@@ -1,8 +1,13 @@
 # Settings specific to the AMD7640U laptop.
+{ inputs, pkgs, ... }:
+
 {
   imports = [
+    inputs.nixos-hardware.nixosModules.framework-13-7040-amd
     ./hardware-configuration.nix
   ];
 
   networking.hostName = "amd7640u";
+
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 }
